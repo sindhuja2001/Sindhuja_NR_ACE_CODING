e24f2d0f5d8d013224693ebe12ca9b7e6650503e
sindhuja2001 <sindhuja20112001@gmail.com>
2021-09-29 06:09:37 +0530

    Day 1

---
+++ Datatypes.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a= 10;
+    cout<<a;
+    cout<<"\n";
+    char b= 'A';
+    cout<< b;
+     cout<<"\n";
+    float c= 10.012345;
+    cout<< c;
+     cout<<"\n";
+    double d= 10.02;
+    cout<< d;
+     cout<<"\n";
+    string s= "NR_ACECODING";
+    cout<< s;
+}
---
+++ Namespace.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+namespace xyz{
+    int b= 20;
+}
+
+
+int main(){
+    std::cout<<xyz::b;
+}
+
